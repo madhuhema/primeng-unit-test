46d5c7afa9001d76359ecd11a299222f19ef4480
madhuhema <madhunarmi@gmail.com>
2018-07-22 19:00:08 +0530

    primeng menubar with unit testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+// import { MenubarModule } from 'primeng/primeng';
+// import { MenuItem } from 'primeng/components/common/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenubarModule } from 'primeng/menubar';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+import { AccordionModule } from 'primeng/components/accordion/accordion';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+   title = 'app';
+  items: MenuItem[];
+  items1: MenuItem[];
+  index = 0;
+  ngOnInit() {
+      this.items = [
+          {label: 'New', icon: 'fa-plus'},
+          {label: 'Open', icon: 'fa-download'},
+          {label: 'Undo', icon: 'fa-refresh'},
+      ];
+      this.items1 = [
+          {
+            label: 'File',
+            icon: 'fa-file-o',
+            items: [{
+              label: 'New',
+              icon: 'fa-plus',
+              items : [
+                 { label: 'Project'},
+                 { label: 'Other' },
+              ]
+            },
+            { label: 'Open'},
+            { separator: true},
+            { label: 'Quit'}
+          ]
+        },
+        {
+          label: 'Edit',
+          icon: 'fa-edit',
+          items: [
+            { label: 'Undo', icon: 'fa-mail-forward'},
+            { label: 'Redo', icon: 'fa-mail-reply'},
+          ]
+        },
+        {
+          label: 'Help',
+          icon: 'fa-question',
+          items: [
+            { label: 'Contents'},
+            {
+              label: 'Search',
+              icon: 'fa-search',
+              items: [
+                  {
+                    label: 'Text',
+                    items: [
+                      {
+                        label: 'Workspace'
+                      }
+                    ]
+                  },
+                  {
+                    label: 'File'
+                  }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Actions',
+          icon: 'fa-gear' ,
+          items: [
+            {
+              label: 'Edit',
+              icon: 'fa-refresh',
+              items: [
+                {label: 'Save', icon: 'fa-save'},
+                {label: 'Update', icon: 'fa-save'}
+              ]
+            },
+            {
+              label: 'Other',
+              icon: 'fa-phone',
+              items: [
+                { label: 'Delete', icon: 'fa-minus'}
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Quit', icon: 'fa-minus'
+        }
+      ];
+  }
+  onLogOutButtonClick() {
+    return  'User Logged Out' ;
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+// import { MenuItem } from 'primeng/components/common/api';
+import { MenubarModule } from 'primeng/menubar';
+import {MenuModule} from 'primeng/menu';
+import {MenuItem} from 'primeng/api';
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
-
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { PanelMenuModule } from 'primeng/panelmenu';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularFontAwesomeModule,
+    AccordionModule,
+    MenuModule,
+    MenubarModule,
+    PanelMenuModule
+],
+ schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
