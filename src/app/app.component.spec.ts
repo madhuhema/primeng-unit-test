596f96498f854cbcae38b7e1f714b2724de4e667
madhuhema <madhunarmi@gmail.com>
2018-07-23 11:41:42 +0530

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { PanelMenuModule } from 'primeng/panelmenu';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AngularFontAwesomeModule,
+        AccordionModule,
+        MenuModule,
+        MenubarModule,
+        PanelMenuModule
+      ]
     }).compileComponents();
   }));
   // test the button click
@@ -13,10 +29,10 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     spyOn(app, 'onLogOutButtonClick');
-      const button = fixture.debugElement.nativeElement.querySelector('button');
+    const button = fixture.debugElement.nativeElement.querySelector('button');
     button.click();
-      fixture.whenStable().then(() => {
-      expect(app.onLogOutButtonClick()).toHaveBeenCalled();
+    fixture.whenStable().then(() => {
+      expect(app.onLogOutButtonClick).toHaveBeenCalled();
     });
   }));
 });
